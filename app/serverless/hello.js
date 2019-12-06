984d7539cd061949f0cc38ba69222a490cdd378c
Colin Tindle <colin.tindle@fanduel.com>
2019-12-06 15:06:38 +0000

    remove token printing

--- app/serverless/hello.js
+++ app/serverless/hello.js
@@ -1,8 +1,6 @@
 exports.handler = function(event, context, callback) {
-    const { identity, user } = context.clientContext
-
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ identity, user })
+        body: "Hello"
     })
 }
