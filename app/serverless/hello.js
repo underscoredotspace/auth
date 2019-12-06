eb0ac389950ff48184a0e952a7e80a283e8bb2cc
Colin Tindle <colin.tindle@fanduel.com>
2019-12-06 16:40:42 +0000

    check for user

--- app/serverless/hello.js
+++ app/serverless/hello.js
@@ -1,8 +1,16 @@
 exports.handler = function(event, context, callback) {
     console.log(JSON.stringify({ event, context }, null, 2))
 
-    callback(null, {
+    const { user } = context.clientContext
+    if (!user) {
+        return callback(null, {
+            statusCode: 401,
+            body: `woops, you shouldn't be here!`
+        })
+    }
+    // Put your custom request to your api backend here.
+    return callback(null, {
         statusCode: 200,
-        body: "Hello"
+        body: JSON.stringify(user)
     })
 }
