869c124589583bc57cd3cb1bfb1eda61c5dc7cbd
Colin Tindle <colin.tindle@fanduel.com>
2019-12-06 14:58:06 +0000

    see what we get from backend

--- app/serverless/hello.js
+++ app/serverless/hello.js
@@ -1,6 +1,8 @@
 exports.handler = function(event, context, callback) {
+    const { identity, user } = context.clientContext
+
     callback(null, {
         statusCode: 200,
-        body: "Hello, World"
+        body: JSON.stringify({ identity, user })
     })
 }
