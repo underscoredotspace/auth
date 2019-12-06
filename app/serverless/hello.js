e1d1742c38f2cb25fa2c2cdced19c395c06f3931
Colin Tindle <colin.tindle@fanduel.com>
2019-12-06 16:22:49 +0000

    add console log

--- app/serverless/hello.js
+++ app/serverless/hello.js
@@ -1,4 +1,6 @@
 exports.handler = function(event, context, callback) {
+    console.log(JSON.stringify({ event, context }, null, 2))
+
     callback(null, {
         statusCode: 200,
         body: "Hello"
